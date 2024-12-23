727d4808b00f14845b11b7d757c1a6146b176efb
Kawser Ahmmed <136078973+kawser25350@users.noreply.github.com>
2024-12-23 11:37:33 +0600

    Update main.py

--- Bank_Managment_System/main.py
+++ Bank_Managment_System/main.py
@@ -2,7 +2,7 @@ from bank import Bank
 from operationManager import OperationManager
 
 if __name__ == "__main__":
-    bank = Bank('Bank of America', 'New York', 1000000)
+    bank = Bank('bangladesh bank', 'New York', 1000000)
     operation = OperationManager()
     operation.main_menu(bank)
 
